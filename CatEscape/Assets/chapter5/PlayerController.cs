446cf980b4b1621f0607c6bdcde14a95ddb184b4
Unknown <aiyuixps13@outlook.jp>
2018-06-04 18:26:37 +0900

    コミット

---
+++ CatEscape/Assets/chapter5/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+
+    GameObject player;
+
+	// Use this for initialization
+	void Start () {
+        this.player = GameObject.Find("player");
+	}
+
+
+
+    public void LButtonDown()
+    {
+        transform.Translate(-3, 0, 0);
+    }
+    public void RButtonDown()
+    {
+        transform.Translate(3, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.Translate(-3, 0, 0);
+        }
+        if(Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.Translate(3, 0, 0);
+        }
+
+
+    }
+}
